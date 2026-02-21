70f341cd027b0b2fb5edeef34fc6e9941ce9af03
tamai-hideyuki <hide27yuki@gmail.com>
2026-02-21 16:48:13 +0900

    改行追加

--- src/http.ts
+++ src/http.ts
@@ -1,5 +1,6 @@
 export const fetchPage = async (url: string): Promise<string> => {
   const response = await fetch(url);
   if (!response.ok) throw new Error(`HTTP Error: ${response.status}`);
+
   return response.text();
 };
