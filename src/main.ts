79ff99fcfde64bcac91478b90d5d7b6c424e0b03
tamai-hideyuki <hide27yuki@gmail.com>
2026-02-21 16:05:58 +0900

    コメント追加

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { tokenize } from "./tokenizer.ts";
 import { buildTree } from "./tree.ts";
 import { extractText } from "./render.ts";
 
+/**
+ * argument vector の略
+ * 
+ * arg = argument（引数）
+ * v = vector（配列・ベクトル）
+ * 
+ * つまり「引数の配列」という意味
+ * これは Node.js 独自の命名ではなく、C言語の main 関数に由来
+ */
 const url = process.argv[2];
 
 if (!url) {
